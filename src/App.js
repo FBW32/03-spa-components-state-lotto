bed335eebd690a6c3175754071ff2bc04539acdf
Gabriele Pepe <gabriele.pepe@gmail.com>
2020-11-02 10:12:04 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 
 import React from 'react';
+import Numbers from './Numbers';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {            /* in the state we store our data */
+    number :  [],
+    reset : false
+  }
+
+randomNumber = () => {  
+let num = 0;
+let myArr = []
+  for (let i=0; i< 7; i++){
+    if (i === 6){
+      ( num= Math.ceil(Math.random() * 10 ))
+    }else {
+      ( num= Math.ceil(Math.random() * 50 ))
+      while(myArr.includes(num)) {
+        ( num= Math.ceil(Math.random() * 50 ))
+      }
+    } myArr.push(num)
+  }  
+this.setState({number : myArr})
+ 
+} 
+
+
+
+hideNumber=()=> {
+
+  this.setState({number: [], reset: false})
+}
+
+
+  render(){
+    return (
+      <div>
+        <h1>Lotto 6/49</h1>
+        <h3>Generating lucky numbers</h3>
+        {this.state.reset ? null : <div style={{display: "flex"}}> {this.state.number.map(num => {
+         return (
+ <Numbers number = {num} />
+         )
+       })} </div>}
+       
+        <button onClick={this.hideNumber}>Reset</button>
+        <button onClick={this.randomNumber}>Show me lucky numbers</button>
+      </div>
+    )
+  }
+
+
 }
 
 
